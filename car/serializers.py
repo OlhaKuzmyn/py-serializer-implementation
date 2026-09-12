7d6f7a658e4ab7db0395cc4d8bf8980810fc1a6e
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-12 17:46:07 +0300

    fixed line

--- car/serializers.py
+++ car/serializers.py
@@ -8,7 +8,9 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64, required=True)
     model = serializers.CharField(max_length=64, required=True)
-    horse_power = serializers.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1914)], required=True)
+    horse_power = serializers.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(1914)], required=True
+    )
     is_broken = serializers.BooleanField(required=True)
     problem_description = serializers.CharField(required=False)
 
