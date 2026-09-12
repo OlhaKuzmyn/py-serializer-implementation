d4bdc4ec0f875eaf95e2a795b9db8543f511a2e2
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-12 18:19:10 +0300

    fixed typo

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def deserialize_car_object(json: bytes) -> Car:
     serializer = CarSerializer(data=data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
-    new_bus =  serializer.save()
-    return new_bus
+    new_car =  serializer.save()
+    return new_car
